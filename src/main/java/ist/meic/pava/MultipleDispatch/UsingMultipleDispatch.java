8bef0f4932e51b631d5c4c3d27e3eeb561df409b
Diogo Pacheco <dmpacheco1999@gmail.com>
2021-04-05 23:52:47 +0000

    partial done

--- src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
+++ src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
@@ -1,6 +1,9 @@
 package ist.meic.pava.MultipleDispatch;
 
 import java.util.List;
+
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
 import java.util.ArrayList;
 
 import java.lang.reflect.Method;
@@ -13,58 +16,111 @@ public class UsingMultipleDispatch {
 	public static Object invoke(Object receiver, String name, Object... args) {
 		try {
 
-			List<Class> argsClasses = new ArrayList<Class>();
+			List<Class> argsType = new ArrayList<Class>();
 
 			for (Object arg : args) {
-				argsClasses.add(arg.getClass());
+				argsType.add(arg.getClass());
+				//System.out.println("argo: " + arg);
 			}
 
-			List<Method> availableMethods = new ArrayList<Method>();
+			List<Method> availableMethods = getAvailableMethods(receiver, name, argsType);
 
-			for (Method m : receiver.getClass().getMethods()) { // getMethods
-				if (name.equals(m.getName()) && m.getParameters().length == args.length) {
-					availableMethods.add(m);
-					/*System.out.println("<Method>");
-					System.out.println("  " + m);
-					System.out.println("    <Parameter>");
-					for (Parameter p : m.getParameters()) {
-						System.out.println("      " + p.getType());
-					}
-					System.out.println("    </Parameter>");
-					System.out.println("</Method>");*/
-				}
-			}
-
-			Class[] argsType = argsClasses.toArray(new Class[argsClasses.size()]);
-			for (Class a : argsType) {
-				System.out.println(a);
-			}
-
-			Method method = bestMethod(receiver.getClass(),	name, argsClasses.toArray(new Class[argsClasses.size()]));
+			Method method = bestMethod(availableMethods);
 			return method.invoke(receiver, args);
 		} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public static Method bestMethod(Class type, String name, Class... argsType)
-	  throws NoSuchMethodException {
-		try {
-			return type.getMethod(name, argsType);
-		} catch (NoSuchMethodException e) { // fazer todas as permutaçoes possiveis e escolher a que tem super class
-			List<Class> argsTypesSuperClasses = new ArrayList<>();
+	public static boolean compatibleParameters(List<Class> partypes, List<Class> argstypes) {
+		int i;
+		for(i = 0; i < argstypes.size(); i++) {
+			Class type = argstypes.get(i);
+			while(type != Object.class && type != partypes.get(i)) {
+				type = type.getSuperclass();
+			}
+			if (partypes.get(i) != type) {
+				break;
+			}
+		}
+		return i == argstypes.size();
+	}
 
-			// System.out.println(argsType.getDeclaredFields());
+	public static List<Method> getAvailableMethods(Object receiver, String name, List<Class> argsType) {
+		List<Method> availableMethods = new ArrayList<Method>();
 
-			for (Class argType : argsType) {
-				if (argType == Object.class) {
-					throw e;
-				} else {
-					argsTypesSuperClasses.add(argType.getSuperclass());
+		for (Method m : receiver.getClass().getMethods()) { 
+			if (name.equals(m.getName()) && m.getParameters().length == argsType.size()) {
+
+				List<Class> parTypes = new ArrayList<Class>();
+
+				for(Parameter p : m.getParameters()){
+					parTypes.add(p.getType());
+				}
+
+				if (compatibleParameters(parTypes, argsType)) {
+					availableMethods.add(m);
+					//System.out.println("method: " + m);
 				}
 			}
+		}
 
-			return bestMethod(type, name, argsTypesSuperClasses.toArray(new Class[argsTypesSuperClasses.size()]));
+		return availableMethods;
+	}
+
+	public static Method chooseBest(Method m1, Method m2) {
+		Class type;
+		
+		//System.out.println(m1 + " vs " + m2);
+		//System.out.println(m1.getDeclaringClass() + " vst " + m2.getDeclaringClass());
+		if (m1.getDeclaringClass() == m2.getDeclaringClass()) {
+			int i;
+			for(i = 0; i < m1.getParameters().length; i++) {
+
+				Class m1Type = m1.getParameters()[i].getType();
+				Class m2Type = m2.getParameters()[i].getType();
+
+				if (m1Type == m2Type) {
+					//System.out.println("entrou1");
+					continue;
+				}
+				while(m1Type != Object.class && m1Type != m2Type) {
+					m1Type = m1Type.getSuperclass();
+				}
+				if (m2Type != m1Type) {
+					//System.out.println("entrou2");
+					return m2;
+				}
+				//System.out.println("entrou3");
+				return m1;
+			}
+			return m1;
+		}
+		type = m1.getDeclaringClass();
+		while(type != Object.class && type != m2.getDeclaringClass()) {
+			type = type.getSuperclass();
+		}
+		if (type == m2.getDeclaringClass()){
+			return m1;
+		}
+		return m2;
+	}
+
+
+	public static Method bestMethod(List<Method> availableMethods)
+	  throws NoSuchMethodException {
+		try {
+			if (availableMethods.isEmpty()) {
+				throw new NoSuchMethodException();
+			}
+			Method best = availableMethods.get(0);
+			int i;
+			for (i = 1; i < availableMethods.size(); i++) {
+				best = chooseBest(best, availableMethods.get(i));
+			}
+			return best;
+		} catch (NoSuchMethodException e) {
+			throw e;
 		}
 	}
 }
--- src/main/java/ist/meic/pava/app/App.java
+++ src/main/java/ist/meic/pava/app/App.java
@@ -11,7 +11,7 @@ import java.lang.ClassNotFoundException;
 public class App {
 	public static void main(String[] args) {
 		printer();
-		reflection();
+		//reflection();
 	}
 
 	public static void printer() {
